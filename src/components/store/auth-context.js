c15ebca93a8175ea0c44f59aec172dfce58e0387
hftamayo <hftamayo@gmail.com>
2022-09-22 15:59:17 -0600

    adding token prop to auth-ctx

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const AuthContext = React.createContext({
+  token: '',
   isLoggedIn: false,
   onLogout: () => {},
   onLogin: (email, password) => {}
