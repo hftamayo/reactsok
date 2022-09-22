9d731d1e716ded56cf61311fb3187abfd20be72b
hftamayo <hftamayo@gmail.com>
2022-09-22 10:03:39 -0600

    adding connection params for signup handshake

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -5,6 +5,7 @@ import Input from "../UI/Input/Input";
 import HeaderButton from "../UI/Buttons/HeaderButton";
 
 const Signup = (props) => {
+  const FB_KEY = process.env.SOK_FBASE_API_KEY;
   const [firstNameValue, setFirstNameValue] = useState("");
   const [lastNameValue, setLastNameValue] = useState("");
   const [emailClientValue, setEmailClientValue] = useState("");
@@ -54,7 +55,7 @@ const Signup = (props) => {
       returnSecureToken: true,
     };
 
-    const response = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]", {
+    const response = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={FB_KEY}", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
