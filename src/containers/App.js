289aaa875fa6654f5d0ff24f5e8a75b97129e745
hftamayo <hftamayo@gmail.com>
2022-09-23 11:00:04 -0600

    minor changes on index and app js files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+
 import Sok from "../components/Sok";
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { ReactDOM, createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./containers/App";
 import "./index.css";
