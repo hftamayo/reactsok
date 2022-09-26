ba67e07b67432c6fd9ca6c498244db9d972c7106
hftamayo <hftamayo@gmail.com>
2022-09-26 12:25:09 -0600

    SignUp: data input validation part1

--- src/components/Auth/SignupForm.jsx
+++ src/components/Auth/SignupForm.jsx
@@ -7,11 +7,7 @@ const isEmpty = (value) => value.trim() === "";
 const isNotSevenChars = (value) => value.trim().length !== 7;
 const notEqualPasswords = (value1, value2) => value1.trim() === value2.trim();
 
-const Signup = (props) => {
-  const FB_KEY = process.env.SOK_FBASE_API_KEY;
-  const SIGNUP_URL =
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={FB_KEY}";
-
+const SignupForm = (props) => {
   const [formInputsValidity, setFormInputsValidity] = useState({
     firstName: true,
     lastName: true,
@@ -25,102 +21,24 @@ const Signup = (props) => {
   const password1ClientRef = useRef();
   const password2ClientRef = useRef();
 
-  const [isCanceling, setIsCanceling] = useState(false);
-  const [isSaving, setIsSaving] = useState(false);
-  const [didSave, setDidSave] = useState(false);
-  const [isErrorOnSave, setIsErrorOnSave] = useState(false);
-  const [errorOnSaveMessage, setErrorOnSaveMessage] = useState("");
-  //const cartCtx = useContext(CartContext);
+
+  const submitHandler = (event) => {
+      event.prevent.Default();
+
+      const enteredFirstName = firstNameRef.current.value;
+      const enteredLastName = lastNameRef.current.value;
+      const enteredEmail = emailClientRef.current.value;
+      const enteredPassword1 = password1ClientRef.current.value;
+      const enteredPassword2 = password2ClientRef.current.value;
+
+      const enteredFirstNameIsValid = !isEmpty(enteredFirstName);
+      const enteredLastNameIsValid = !isEmpty(enteredLastName);
+      const enteredEmailIsValid = !isEmpty(enteredEmail);
+      const enteredPasswordLength = !isNotSevenChars(enteredPassword1);
+      const enteredConfirmPassword = !notEqualPasswords(enteredPassword1, enteredPassword2);
 
 
-  const errorOnSignupHandler = (errorDescription) => {
-    setErrorOnSaveMessage(errorDescription);
-    setIsErrorOnSave(true);
   };
-
-  const signupHandler = async () => {
-    setIsSaving(true);
-    //showSpinner = true
-    const enteredFirstname = firstNameValue;
-    const enteredLastname = lastNameValue;
-    const enteredEmail = emailClientValue;
-    const enteredPassword = passwordClientValue;
-
-    const newClientData = {
-      //firstname: enteredFirstname,
-      //lastname: enteredLastname,
-      email: enteredEmail,
-      password: enteredPassword,
-      returnSecureToken: true,
-    };
-
-    const response = await fetch(SIGNUP_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newClientData),
-    });
-
-    //showSpinner = false
-    if (!response.ok) {
-      errorOnSignupHandler(response.data);
-    } else {
-      setIsSaving(false);
-      setIsCanceling(false);
-      setDidSave(true);
-      //cartCtx.clearCart();
-    }
-  };
-
-  const isSavingModalContent = <p>Saving new user...</p>;
-  /* incluir transaccion para verificar si es exitoso o hubo algun error */
-
-  const errorOnSavingModalContent = (
-    <React.Fragment>
-      <p>
-        The user account could not be created. Please report the next error:
-      </p>
-      <p>{errorOnSaveMessage}</p>
-      <div className={classes.actions}>
-        <button className={classes.button} onClick={props.onClose}>
-          Close
-        </button>
-      </div>
-    </React.Fragment>
-  );
-
-  const didSaveModalContent = (
-    <React.Fragment>
-      <p>User account created, welcome!</p>
-      <div className={classes.actions}>
-        <button className={classes.button} onClick={props.onClose}>
-          Close
-        </button>
-      </div>
-    </React.Fragment>
-  );
-
-  const SignupButtons = (
-    <React.Fragment>
-      <div className={classes.btncontainer}>
-        <HeaderButton
-          onClick={signupHandler}
-          userIcon={1}
-          requestedLabel="Login"
-        />
-        <HeaderButton
-          onClick={props.onClose}
-          userIcon={0}
-          requestedLabel="Close"
-        />
-      </div>
-    </React.Fragment>
-  );
-
-  const modalActions = (
-    <div className={classes.actions}>{!isCanceling ? SignupButtons : ""}</div>
-  );
 
   const SignupModalContent = (
     <React.Fragment>
