db188cc22a6c52b95db8c1775e12f5f31ca041ee
hftamayo <hftamayo@gmail.com>
2022-10-17 11:06:09 -0600

    minor change on provider

--- src/components/store/AuthProvider.js
+++ src/components/store/AuthProvider.js
@@ -8,7 +8,6 @@ export const AuthProvider = (props) => {
 
   const loginTmpHandler = () => {
     setIsLoggedIn(true);
-    console.log("estado de la flag isLoggedIn: " + isLoggedIn);
   }
 
   return (
