f28465e75395d9a22033e06854e6537e20b00fc5
hftamayo <hftamayo@gmail.com>
2022-10-19 09:49:40 -0600

    adding autofocus prop to the first control

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -142,6 +142,7 @@ const Login = (props) => {
         label="E-Mail"
         type="email"
         autodata="off"
+        focus={true}        
       />
       <Input
         onChange={passwordValueHandler}
