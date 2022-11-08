57da4635872e565c1e57ef34f9e17cf08fb31bb0
hftamayo <hftamayo@gmail.com>
2022-11-08 09:04:33 -0600

    minor updates related to language tags

--- i18n.js
+++ i18n.js
@@ -1,17 +1,19 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 
 i18n
-.use(Backend)
-.use(initReactI18next)
-.init({
-    fallbackLng: 'en',
+  .use(I18nextBrowserLanguageDetector)
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
     debug: true,
 
     interpolation: {
-        escapeValue: false
-    }
-});
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,4 @@
 import React, { Suspense } from "react";
-import { useTranslation } from "react-i18next";
 import Sok from "../components/Sok";
 
 const App = () => {
