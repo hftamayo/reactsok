878f7251e7a67c8c3c041960d41e6f00a1955c3f
hftamayo <hftamayo@gmail.com>
2022-11-09 11:32:11 -0600

    cleaning app component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,10 @@
-import React, { Suspense } from "react";
+import React from "react";
 import i18n from "../i18n";
 import Sok from "../components/Sok";
 
 const App = () => {
   return (
-    //<Suspense fallback= {<Loading />}>
-    <Suspense>
       <Sok />
-    </Suspense>
   );
 };
 export default App;
