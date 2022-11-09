b4aa3b693f89ed5587cc751856d72cc01db25f31
hftamayo <hftamayo@gmail.com>
2022-11-09 12:29:35 -0600

    rolling back calls from public_locales

--- src/i18n.js
+++ src/i18n.js
@@ -3,25 +3,13 @@ import { initReactI18next } from "react-i18next";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
-import translationEN from "../public/locales/en/translation.json";
-import translationSP from "../public/locales/sp/translation.json";
-//import translationFR from '../public/locales/fr/translation.json';
-
-const resources = {
-  en: {
-    translation: translationEN,
-  },
-  sp: {
-    translation: translationSP,
-  },
-};
+//backend is a lazy loading and search for translation on public/locales
 
 i18n
   .use(I18nextBrowserLanguageDetector)
   .use(Backend)
   .use(initReactI18next)
   .init({
-    resources,
     fallbackLng: "en",
     debug: true,
 
