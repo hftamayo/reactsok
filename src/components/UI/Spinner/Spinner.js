daf2b2d5036157bd8e043f746cb8547869f776e3
hftamayo <hftamayo@gmail.com>
2022-11-09 15:07:35 -0600

    center the spinner vert and horizontally

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ClipLoader from 'react-spinners/ClipLoader';
+/* https://www.davidhu.io/react-spinners/ */
+import ClipLoader from "react-spinners/BounceLoader";
 
 function Spinner() {
   return (
-    <div style={{ width: '100px', margin: 'auto', display: 'block' }}>
-      <ClipLoader color="#52bfd9" size={100}/>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <ClipLoader color="#52bfd9" size={100} />
     </div>
   );
-};
+}
 
 export default Spinner;
