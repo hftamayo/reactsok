0eccd165a5940ac53158d1dae3a0edc19ef9cf21
hftamayo <hftamayo@gmail.com>
2022-11-09 12:23:22 -0600

    updating i18n for testing of the spinner

--- src/i18n.js
+++ src/i18n.js
@@ -3,11 +3,25 @@ import { initReactI18next } from "react-i18next";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
+import translationEN from "../public/locales/en/translation.json";
+import translationSP from "../public/locales/sp/translation.json";
+//import translationFR from '../public/locales/fr/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  sp: {
+    translation: translationSP,
+  },
+};
+
 i18n
   .use(I18nextBrowserLanguageDetector)
   .use(Backend)
   .use(initReactI18next)
   .init({
+    resources,
     fallbackLng: "en",
     debug: true,
 
