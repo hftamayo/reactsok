f02ae6c0b9ab29bf7b81052180352ab8e057595e
hftamayo <hftamayo@gmail.com>
2022-11-09 13:00:51 -0600

    pre-alpha version of showing the spinner

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,14 @@
-import React from "react";
-import i18n from "../i18n";
+import React, { Suspense } from "react";
+import Spinner from "../components/UI/Spinner/Spinner";
 import Sok from "../components/Sok";
+import i18n from "../i18n";
 
 const App = () => {
+
   return (
+    <Suspense fallback={<Spinner />}>
       <Sok />
+      </Suspense>      
   );
 };
 export default App;
