efbc298b4df2b3f1448310eec8dd7003af3310c5
hftamayo <hftamayo@gmail.com>
2022-11-14 10:32:03 -0600

    experimenting with MDBCard part1

---
+++ src/components/UI/MDBCard/Card.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardFooter,
+  MDBRow,
+  MDBCol
+} from 'mdb-react-ui-kit';
+
+const Card = (props) => {
+  return (
+    <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src={props.image}
+            alt={props.altImage}
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>{props.cardTitle}</MDBCardTitle>
+            <MDBCardText>
+                {props.cardText}
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small className='text-muted'>{props.cardFooter}</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src='https://mdbootstrap.com/img/new/standard/city/043.webp'
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This card has supporting text below as a natural lead-in to additional content.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src='https://mdbootstrap.com/img/new/standard/city/042.webp'
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This
+              card has even longer content than the first to show that equal height action.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
+  );
+}
+
+export default Card;
