b298063022a28dcdc09e0d1deef475d714faa076
hftamayo <hftamayo@gmail.com>
2022-11-17 10:25:06 -0600

    adding another contextprovider as a test

---
+++ src/components/store/GlobalState.js
@@ -0,0 +1,53 @@
+import React, { createContext, useReducer } from "react";
+import CategoriesReducer from "./CategoriesReducer";
+
+const initialState = {
+  categories: [
+    {
+      id: 1,
+      name: "Cybersecurity",
+      description: "Pentesting, Offensive, Defensive, DFIR, Malware",
+      status: 1,
+    },
+  ],
+};
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(CategoriesReducer, initialState);
+
+  function addCategory(category) {
+    dispatch({
+      type: "ADD_CATEGORY",
+      payload: category,
+    });
+  }
+
+  function editCategory(category) {
+    dispatch({
+      type: "EDIT_CATEGORY",
+      payload: category,
+    });
+  }
+
+  function removeCategory(id) {
+    dispatch({
+      type: "REMOVE_CATEGORY",
+      payload: id,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        categories: state.categories,
+        addCategory,
+        editCategory,
+        removeCategory,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
