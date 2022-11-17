c1b16a1cf660f3d0a778fdaab55497c87d16d1ac
hftamayo <hftamayo@gmail.com>
2022-11-17 10:35:56 -0600

    displaying records part1

---
+++ src/components/Categories/ViewCategories.jsx
@@ -0,0 +1,25 @@
+import React, { useContext, Fragment } from "react";
+import { GlobalContext } from "../store/GlobalState";
+
+const ViewCategories = () => {
+  const { categories } = useContext(GlobalContext);
+  return (
+    <Fragment>
+      {categories.length > 0 ? (
+        <Fragment>
+          {categories.map((employee) => (
+            <div key={category.id}>
+              <div>
+                <p>{category.name}</p>
+                <p>{category.description}</p>
+                <span>{category.status}</span>
+              </div>
+            </div>
+          ))}
+        </Fragment>
+      ) : (
+        <p>No records to display</p>
+      )}
+    </Fragment>
+  );
+};
