a65ac4046b3bd91b637c8369554c88c61eb46842
hftamayo <hftamayo@gmail.com>
2022-11-17 09:28:00 -0600

    reducer for categories CRUD operation

--- src/components/store/CategoriesReducer.js
+++ src/components/store/CategoriesReducer.js
@@ -1,5 +1,37 @@
 const CategoriesReducer = () => {
+  switch (action.type) {
+    case "ADD_CATEGORY":
+      return {
+        ...state,
+        categories: [...state.categories, action.payload],
+      };
 
-}
+    case "EDIT_CATEGORY":
+      const updatedCategory = action.payload;
+
+      const updatedCategories = state.categories.map((category) => {
+        if (category.id === updatedCategory.id) {
+          return updatedCategory;
+        }
+        return category;
+      });
+
+      return {
+        ...state,
+        categories: updatedCategories,
+      };
+
+    case "REMOVE_EMPLOYEE":
+      return {
+        ...state,
+        categories: state.categories.filter(
+          (category) => category.id !== action.payload
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default CategoriesReducer;
