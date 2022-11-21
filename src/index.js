e47450b2e7135ab81e5619e6883ff66858f06a3e
hftamayo <hftamayo@gmail.com>
2022-11-21 11:44:02 -0600

    removing unneeded browser router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./containers/App";
 import "./index.css";
 import { AuthProvider } from "./components/store/AuthProvider";
@@ -8,8 +7,6 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <AuthProvider>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </AuthProvider>
 );
