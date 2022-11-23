4448c9475c94359e1df2a50cc84aa86e795000cc
hftamayo <hftamayo@gmail.com>
2022-11-23 14:54:33 -0600

    adjusting routes

--- src/components/Home/ToolBar.js
+++ src/components/Home/ToolBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Card from "../UI/Card/Card";
 import ViewCategories from "../Categories/ViewCategories";
 import GlobalProvider from "../store/GlobalState";
 import classes from "./Home.module.css";
@@ -8,35 +7,33 @@ import classes from "./Home.module.css";
 const Dashboard = () => {
   return (
     <BrowserRouter>
-      <div className={classes.home}>
-        <Card>
-          <h1>
-            <Link to="/categories">Categories</Link>
-          </h1>
-        </Card>
-        <Card>
-          <h1>
-            <Link to="/newasset">New Asset</Link>
-          </h1>
-        </Card>
-        <Card>
-          <h1>
-            <Link to="/inventory">Inventory</Link>
-          </h1>
-        </Card>
-        <Routes>
-          <Route
-            path="/categories"
-            element={
-                <GlobalProvider>
-                  <ViewCategories />
-                </GlobalProvider>
-            }
-          />
-          <Route path="/newasset" />
-          <Route path="/inventory" />
-        </Routes>
-      </div>
+      <header className={classes.header}>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+            <li>
+              <Link to="/newasset">New Asset</Link>
+            </li>
+            <li>
+              <Link to="/inventory">Inventory</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Routes>
+        <Route
+          path="/categories"
+          element={
+            <GlobalProvider>
+              <ViewCategories />
+            </GlobalProvider>
+          }
+        />
+        <Route path="/newasset" />
+        <Route path="/inventory" />
+      </Routes>
     </BrowserRouter>
   );
 };
