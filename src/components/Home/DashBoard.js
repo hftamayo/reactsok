ce62123c60ca80f364ee9d8808fc06a5d68737a0
hftamayo <hftamayo@gmail.com>
2022-11-23 14:56:30 -0600

    rebuilding dashboard

---
+++ src/components/Home/DashBoard.js
@@ -0,0 +1,13 @@
+import ToolBar from './ToolBar';
+import classes from './DashBoard.module.css';
+
+function Layout(props) {
+  return (
+    <div>
+      <MainNavigation />
+      <main className={classes.main}>{props.children}</main>
+    </div>
+  );
+}
+
+export default Layout;
