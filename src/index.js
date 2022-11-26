7898f628be8e0db8be406bcfa93d41b6c5b6a4b4
hftamayo <hftamayo@gmail.com>
2022-11-26 17:17:51 -0600

    adding tailwind y browserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./containers/App";
+import { BrowserRouter } from "react-router-dom";
+import "./tailwind.css";
 import "./index.css";
 import { AuthProvider } from "./components/store/AuthProvider";
 
@@ -7,6 +9,8 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <AuthProvider>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </AuthProvider>
 );
