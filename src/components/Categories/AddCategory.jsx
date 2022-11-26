e9a6c7b35ed9942296be5730add35e667af91331
hftamayo <hftamayo@gmail.com>
2022-11-26 17:28:28 -0600

    AddCategory first version

---
+++ src/components/Categories/AddCategory.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import { GlobalContext } from "../store/GlobalState";
+
+export const AddCategory = () => {
+  let history = useHistory();
+
+  const { addCategory, categories } = useContext(GlobalContext);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [status, setStatus] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newCategory = {
+      id: categories.length + 1,
+      name,
+      description,
+      status,
+    };
+    addCategory(newCategory);
+    history.push("/categories");
+  };
+
+  return (
+    <Fragment>
+      <div className="w-full max-w-sm container mt-20 mx-auto">
+        <form onSubmit={onSubmit}>
+          <div className="w-full mb-5">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="name"
+            >
+              Name of category
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:text-gray-600"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              placeholder="Enter a name"
+            />
+          </div>
+          <div className="w-full mb-5">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="description"
+            >
+              Description
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:text-gray-600 focus:shadow-outline"
+              value={description}
+              onChange={(e) => setLocation(e.target.value)}
+              type="text"
+              placeholder="Enter a description"
+            />
+          </div>
+          <div className="w-full mb-5">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="status"
+            >
+              Active?
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:text-gray-600"
+              value={status}
+              onChange={(e) => setDesignation(e.target.value)}
+              type="text"
+              placeholder="Status"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="mt-5 bg-green-400 w-full hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              Add Category
+            </button>
+          </div>
+          <div className="text-center mt-4 text-gray-500">
+            <Link to="/categories">Cancel</Link>
+          </div>
+        </form>
+      </div>
+    </Fragment>
+  );
+};
+
+export default AddCategory;
