5209d32bb8958b04078fc4ecbcae6153c344ffcc
hftamayo <hftamayo@gmail.com>
2022-11-26 16:44:26 -0600

    adding browserrouter hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Suspense } from "react";
+import { BrowserRouter } from "react-router-dom";
 import Spinner from "../components/UI/Spinner/Spinner";
 import Sok from "../components/Sok";
 import i18n from "../i18n";
@@ -6,7 +7,9 @@ import i18n from "../i18n";
 const App = () => {
   return (
     <Suspense fallback={<Spinner />}>
-      <Sok />
+      <BrowserRouter>
+        <Sok />
+      </BrowserRouter>
     </Suspense>
   );
 };
