5b8bf26c1a0c887368b61b103205794f908236c1
hftamayo <hftamayo@gmail.com>
2022-11-26 17:15:13 -0600

    adding tailwind tags

--- src/components/Categories/ViewCategories.jsx
+++ src/components/Categories/ViewCategories.jsx
@@ -8,17 +8,22 @@ const ViewCategories = () => {
       {categories.length > 0 ? (
         <Fragment>
           {categories.map((category) => (
-            <div key={category.id}>
-              <div>
-                <p>{category.name}</p>
-                <p>{category.description}</p>
-                <span>{category.status}</span>
+            <div
+              className="flex items-center bg-gray-100 mb-10 shadow"
+              key={category.id}
+            >
+              <div className="flex-auto text-left px-4 py-2 m-2">
+                <p className="text-gray-900 leading-none">{category.name}</p>
+                <p className="text-gray-600">{category.description}</p>
+                <span className="inline-block text-sm font-semibold mt-1">
+                  {category.status}
+                </span>
               </div>
             </div>
           ))}
         </Fragment>
       ) : (
-        <p>No records to display</p>
+        <p className="text-center bg-gray-100 text-gray-500 py-5">No records to display</p>
       )}
     </Fragment>
   );
