e18005c696c315662b9a4ccdecfb89caa7dcd20c
hftamayo <hftamayo@gmail.com>
2022-11-27 17:12:46 -0600

    adding tailwind tags

--- src/components/Home/MainLayout.js
+++ src/components/Home/MainLayout.js
@@ -1,8 +1,7 @@
-import classes from "./DashBoard.module.css";
 import { Outlet } from "react-router-dom";
 
 function MainLayout() {
-  return <main className={classes.content}><Outlet /></main>;
+  return <div className="container mx-auto"><Outlet /></div>;
 }
 
 export default MainLayout;
