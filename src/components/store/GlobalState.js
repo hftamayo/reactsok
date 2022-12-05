ff38bdb210031f851c1d843ed83f429c56ea4346
hftamayo <hftamayo@gmail.com>
2022-12-05 13:15:22 -0600

    testing blogic without database: read func

--- src/components/store/GlobalState.js
+++ src/components/store/GlobalState.js
@@ -1,48 +1,50 @@
 import React, { createContext, useEffect, useState, useReducer } from "react";
 import CategoriesReducer from "./CategoriesReducer";
 
-const LOGIN_URL =
-  "https://movieserp-default-rtdb.firebaseio.com/categories.json";
+const AvailableCategories =  {
+/*   const LOGIN_URL =
+    "https://movieserp-default-rtdb.firebaseio.com/categories.json";
 
-const [loadedCategories, setLoadedCategories] = useState("");
+  const [loadedCategories, setLoadedCategories] = useState([]);
 
-useEffect(() => {
-  const initialState = async () => {    
-    const response = await fetch(LOGIN_URL, {
-      method: "GET",
-    });
-
-    const responseData = await response.json();
-
-    const fetchedCategories = [];
-
-    for (const key in responseData) {
-      fetchedCategories.push({
-        id: key,
-        name: responseData[key].name,
-        description: responseData[key].description,
-        status: responseData[key].status,
+  useEffect(() => {
+    const fetchingData = async () => {
+      const response = await fetch(LOGIN_URL, {
+        method: "GET",
       });
-    }
-    setLoadedCategories(fetchedCategories);
-  };
-}, []);
 
-/*   const initialState = {
-    loadedCategories: [
-      {
-        id: 1,
-        name: "Cybersecurity",
-        description: "Pentesting, Offensive, Defensive, DFIR, Malware",
-        status: 1,
-      },
-    ],
-  }; */
+      const responseData = await response.json();
 
-export const GlobalContext = createContext(initialState);
+      const fetchedCategories = [];
+
+      for (const key in responseData) {
+        fetchedCategories.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          status: responseData[key].status,
+        });
+      }
+      setLoadedCategories(fetchedCategories);
+    };
+  }, []); */
+
+/*   loadedCategories: [
+    {
+      id: 1,
+      name: "Cybersecurity",
+      description: "Pentesting, Offensive, Defensive, DFIR, Malware",
+      status: 1,
+    },
+  ],   */
+
+  loadedCategories: [],
+};
+
+export const GlobalContext = createContext(AvailableCategories);
 
 export const GlobalProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(CategoriesReducer, initialState);
+  const [state, dispatch] = useReducer(CategoriesReducer, AvailableCategories);
 
   function addCategory(category) {
     dispatch({
@@ -68,7 +70,7 @@ export const GlobalProvider = ({ children }) => {
   return (
     <GlobalContext.Provider
       value={{
-        categories: loadedCategories,
+        categories: state.loadedCategories,
         addCategory,
         editCategory,
         removeCategory,
