c73aeb30a6d0e76b9e1e0ece6e2ea42f8803e37f
hftamayo <hftamayo@gmail.com>
2022-12-16 11:40:29 -0600

    adding equipment routes

--- src/components/Home/DashBoard.js
+++ src/components/Home/DashBoard.js
@@ -7,31 +7,51 @@ import ViewCategories from "../Categories/ViewCategories";
 import AddCategory from "../Categories/AddCategory";
 import EditCategory from "../Categories/EditCategory";
 
+import EquipmentHeader from "../entities/Equipments/EquipmentHeader";
+import ViewEquipments from "../entities/Equipments/ViewEquipments";
+import AddEquipment from "../entities/Equipments/AddEquipment";
+
+import { ToasterContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function DashBoard() {
   return (
     <div>
       <GlobalProvider>
-      <ToolBar />
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route
-            path="/categories"
-            exact
-            element={
-              <div>
-                <CategoryHeader />
-                <ViewCategories />
-              </div>
-            }
-          />
-          <Route path="/add-category" exact element={<AddCategory />} />
-          <Route path="/edit-category/:id" exact element={<EditCategory />} />
+        <ToolBar />
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route
+              path="/categories"
+              exact
+              element={
+                <div>
+                  <CategoryHeader />
+                  <ViewCategories />
+                </div>
+              }
+            />
+            <Route path="/add-category" exact element={<AddCategory />} />
+            <Route path="/edit-category/:id" exact element={<EditCategory />} />
 
-          <Route path="/newasset" />
-          <Route path="/inventory" />
-        </Route>
-      </Routes>
-      </GlobalProvider>      
+            <ToasterContainer position="top-center" />
+            <Route
+              path="/equipments"
+              exact
+              element={
+                <div>
+                  <EquipmentHeader />
+                  <ViewCategories />
+                </div>
+              }
+            />
+            <Route path="/add-equipment" exact element={<AddEquipment />} />
+
+            <Route path="/newasset" />
+            <Route path="/inventory" />
+          </Route>
+        </Routes>
+      </GlobalProvider>
     </div>
   );
 }
