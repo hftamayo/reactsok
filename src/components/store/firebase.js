d8ca44519c1f2e53ec39efdd6c51ef9598e61a86
hftamayo <hftamayo@gmail.com>
2022-12-22 15:11:43 -0600

    adjusting firebase to v8 namespaced

--- src/components/store/firebase.js
+++ src/components/store/firebase.js
@@ -1,11 +1,10 @@
+/* firebase v9 
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase } from "firebase/database";  */
 
-/* import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; 
-import { getDatabase } from "firebase/database";
-*/
+import "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB6fBtXeYqMjuj2L_vzO7NSzDMw0lFP06U",
@@ -15,15 +14,19 @@ const firebaseConfig = {
   messagingSenderId: "616599114265",
   appId: "1:616599114265:web:0b41fb9fc74559a04cd088",
   measurementId: "G-J5JSZK4LG3",
-  databaseURL: "https://sotiria.firebaseio.com",
+  /* databaseURL: "https://sotiria-f6005-default-rtdb.firebaseio.com/", */
 };
 
+
+/* firebase V9: 
 const app = initializeApp(firebaseConfig);
-const fireDb = getDatabase(app);
-
-/* const analytics = getAnalytics(fireDb);
-const db = getFirestore(fireDb);
-const fireDb = getDatabase(app);
-export { db }; */
-
+const fireDb = getDatabase(app);  
 export default {fireDb};
+*/
+
+const fireDb = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(fireDb);
+
+export default fireDb.database().ref();
+
+
