763499f41e94249eb9f46ccaac03bd76648572db
hftamayo <hftamayo@gmail.com>
2022-12-31 11:22:30 -0600

    shorting name for opsystems

--- src/components/Home/ToolBar.js
+++ src/components/Home/ToolBar.js
@@ -14,7 +14,7 @@ const ToolBar = () => {
               <Link to="/equipments">Equipments</Link>
             </li>            
             <li>
-              <Link to="/oses">Operating Systems</Link>
+              <Link to="/oses">OpSys</Link>
             </li>                        
             <li>
               <Link to="/newasset">Assets</Link>
