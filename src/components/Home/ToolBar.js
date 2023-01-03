31a25a6b1496075759a2053e5fe612f7e0365aa6
hftamayo <hftamayo@gmail.com>
2023-01-03 09:54:32 -0600

    minor update on links

--- src/components/Home/ToolBar.js
+++ src/components/Home/ToolBar.js
@@ -4,30 +4,33 @@ import classes from "./DashBoard.module.css";
 
 const ToolBar = () => {
   return (
-      <header className={classes.toolbar}>
-        <div>
-          <ul>
-            <li>
-              <Link to="/equipments">Equipments</Link>
-            </li>            
-            <li>
-              <Link to="/oses">OpSys</Link>
-            </li>
-            <li>
-              <Link to="/categories">Categories</Link>
-            </li>
-            <li>
-              <Link to="/subcategories">SubCat</Link>
-            </li>                                                
-            <li>
-              <Link to="/newasset">Assets</Link>
-            </li>
-            <li>
-              <Link to="/inventory">Inventory</Link>
-            </li>
-          </ul>
-        </div>
-      </header>
+    <header className={classes.toolbar}>
+      <div>
+        <ul>
+          <li>
+            <Link to="/twcats">TailWind Cats</Link>
+          </li>
+          <li>
+            <Link to="/equipments">Equipments</Link>
+          </li>
+          <li>
+            <Link to="/oses">OpSys</Link>
+          </li>
+          <li>
+            <Link to="/categories">Cats</Link>
+          </li>
+          <li>
+            <Link to="/subcategories">SubCat</Link>
+          </li>
+          <li>
+            <Link to="/newasset">Assets</Link>
+          </li>
+          <li>
+            <Link to="/inventory">Inventory</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 };
 
