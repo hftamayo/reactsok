534b0b08e361f0af430a459f2a5c24098ad34310
hftamayo <hftamayo@gmail.com>
2023-01-04 10:14:49 -0600

    fixing a typo in the route

--- src/components/entities/SubCategories/SubCatHeader.js
+++ src/components/entities/SubCategories/SubCatHeader.js
@@ -9,7 +9,7 @@ export const SubCatHeader = () => {
           <h5 className="text-gray-900 font-bold text-xl">Sub-Categories Listing</h5>
         </div>
         <div className="flex-grow text-right px-4 py-2 m-2">
-          <Link to="/add-os">
+          <Link to="/add-subcat">
             <button className="bg-green-400 hover:bg-green-500 text-white font-semibold py-2 px-4 rounded inline-flex items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
