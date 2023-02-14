1dcdd1a611953b4356f99d83449992c8e6d22d3c
hftamayo <hftamayo@gmail.com>
2023-02-14 15:04:59 -0600

    updating a typo in a route

--- src/components/entities/Assets/AssetHeader.js
+++ src/components/entities/Assets/AssetHeader.js
@@ -9,7 +9,7 @@ export const AssetHeader = () => {
           <h5 className="text-gray-900 font-bold text-xl">Available Assets List</h5>
         </div>
         <div className="flex-grow text-right px-4 py-2 m-2">
-          <Link to="/add-os">
+          <Link to="/add-asset">
             <button className="bg-green-400 hover:bg-green-500 text-white font-semibold py-2 px-4 rounded inline-flex items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
