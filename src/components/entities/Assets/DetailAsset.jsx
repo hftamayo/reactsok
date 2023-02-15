808901b60a139adf3d69b6979ea3e0a9fc1e5275
hftamayo <hftamayo@gmail.com>
2023-02-15 14:33:44 -0600

    removing unneeded hook

--- src/components/entities/Assets/DetailAsset.jsx
+++ src/components/entities/Assets/DetailAsset.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import fireDb from "../../store/firebase";
 import classes from "../Cruds.module.css";
 
