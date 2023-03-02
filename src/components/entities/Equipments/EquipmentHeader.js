e1bb99d35f06eaf6bf030aadc7b130a16c88ef7f
hftamayo <hftamayo@gmail.com>
2023-03-02 10:51:13 -0600

    removing some spaces inside tags

--- src/components/entities/Equipments/EquipmentHeader.js
+++ src/components/entities/Equipments/EquipmentHeader.js
@@ -5,13 +5,13 @@ import classes from "../Cruds.module.css";
 export const EquipmentHeader = () => {
   return (
     <div>
-      <div >
-        <div >
-          <h5 >Equipment Listing</h5>
+      <div>
+        <div>
+          <h5>Equipment Listing</h5>
         </div>
-        <div >
+        <div>
           <Link to="/add-equipment">
-            <button className={classes.btn + ' ' + classes.btn_new}>
+            <button className={classes.btn + " " + classes.btn_new}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
