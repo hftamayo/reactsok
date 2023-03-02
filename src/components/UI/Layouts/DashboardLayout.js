8c4517db0df394cbef987c2542b751bf7740a7af
hftamayo <hftamayo@gmail.com>
2023-03-02 11:38:25 -0600

    adding footer component

--- src/components/UI/Layouts/DashboardLayout.js
+++ src/components/UI/Layouts/DashboardLayout.js
@@ -1,10 +1,12 @@
 import MainHeader from "../../MainHeader/MainHeader";
+import MainFooter from "../../MainFooter/MainFooter";
 
 const DashboardLayout = () => {
   return (
     <>
       <MainHeader />
       <main>{props.children}</main>
+      <MainFooter />
     </>
   );
 };
