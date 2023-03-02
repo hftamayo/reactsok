35dd90ce473327d4b35cbc9e45fbbf2b46258c6a
hftamayo <hftamayo@gmail.com>
2023-03-02 11:20:21 -0600

    removing header from main component

--- src/components/Sok.js
+++ src/components/Sok.js
@@ -1,11 +1,9 @@
 import React, { Fragment } from "react";
-import Header from "./Header/Header";
 import Home from "./Home/Home";
 
 const Sok = () => {
   return (
     <Fragment>
-      <Header />
       <Home />
     </Fragment>
   );
