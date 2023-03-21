cb22f62306a5a783e7479c0d94dcfc0208fe1424
hftamayo <hftamayo@gmail.com>
2023-03-21 10:29:09 -0600

    fixing status display on Equipments CRUD

--- src/components/entities/Equipments/ViewEquipments.jsx
+++ src/components/entities/Equipments/ViewEquipments.jsx
@@ -77,7 +77,7 @@ firebase v9
                 <td>{data[id].name}</td>
                 <td>{data[id].description}</td>
                 <td>{data[id].brand}</td>
-                <td>{data[id].status}</td>
+                <td>{data[id].status === '1' ? "Active" : "Inactive"}</td>
                 <td>
                   <Link to={`/edit-equipment/${id}`}>
                     <button className={classes.btn + ' ' + classes.btn_edit}>Edit</button>
