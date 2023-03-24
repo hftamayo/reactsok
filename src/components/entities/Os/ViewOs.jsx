d8bad3e7031d62e3c1f6abdcf23b1a3a9770f71c
hftamayo <hftamayo@gmail.com>
2023-03-24 11:19:20 -0600

    removing unneeded hook

--- src/components/entities/Os/ViewOs.jsx
+++ src/components/entities/Os/ViewOs.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import fireDb from "../../store/firebase";
 /* import { ref, child, get } from "firebase/database"; */
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import classes from "../Cruds.module.css";
 import { toast } from "react-toastify";
 
